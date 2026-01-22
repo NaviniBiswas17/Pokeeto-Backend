0ee2e369374e4c75afb315db21906751a8980f06
pranjal-git290504 <pranjalchoudhary1232@gmail.com>
2026-01-22 16:29:25 +0530

    add KidGoal model with mass assignable attributes

---
+++ app/Models/KidGoal.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class KidGoal extends Authenticatable
+{
+    protected $table = 'kid_goals';
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasApiTokens,HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'parent_id',
+        'kid_id',
+        'goal_name',
+        'target_amount',
+        'target_date',
+        'saved_amount',
+        'status'
+    ];
+}
