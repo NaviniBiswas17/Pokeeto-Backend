36a0c3d2961063cbd923396a3bd5ac1d0fcc0577
pranjal-git290504 <pranjalchoudhary1232@gmail.com>
2026-03-16 21:28:42 +0530

    Add relationships for accounts and kids in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Account;
+use App\Models\KidDetail;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -24,5 +27,22 @@ class User extends Authenticatable
         'email',
         'status',
     ];
+
+     // 🔹 User → Accounts
+    public function accounts()
+    {
+        return $this->hasMany(Account::class, 'user_id');
+    }
+
+    // 🔹 User → Kids
+    public function kids()
+    {
+        return $this->hasMany(KidDetail::class, 'parent_id');
+    }
+
+    public function friend_invites()
+    {
+        return $this->hasMany(FriendInvite::class, 'inviter_user_id');
+    }
 
 }
